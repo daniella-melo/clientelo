4bbf0ce676e567509159ab2d7159998beec06cfc
daniella-melo <daniella.cisterna@elo7.com>
2022-11-10 10:45:22 -0300

    mapeia categoria

--- src/main/java/br/com/alura/clientelo/model/Categoria.java
+++ src/main/java/br/com/alura/clientelo/model/Categoria.java
@@ -1,16 +1,27 @@
 package br.com.alura.clientelo.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name = "categoria")
 public class Categoria  {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private int id;
+
+    @Column(name = "nome", nullable = false, unique = true, length = 50)
     private String nome;
 
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
     private CategoriaStatusEnum status;
 
     public Categoria(String nome) {
